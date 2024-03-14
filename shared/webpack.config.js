ecd6891f847d261db31f710f6286703405b65563
ibricks-bizdev <jiminlee@i-bricks.co.kr>
2024-03-14 13:41:04 +0900

    chore: workspace 설정 수정 및 각 레포 추가

--- shared/webpack.config.js
+++ shared/webpack.config.js
@@ -1,32 +1,33 @@
+
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        main: "./src/main.js",
-    },
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "[name].bundle.js",       // remove past build
-        clean: true,
-    },
-    resolve: {
-        extensions: [".js", ".jsx"],
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        static: path.resolve(__dirname, "public"),
-        port: process.env.PORT,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
-            },
-          },
-        ],
-    },
+  mode: "development",
+  entry: {
+    main: "./src/main.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devtool: "source-map",
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: process.env.PORT,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
