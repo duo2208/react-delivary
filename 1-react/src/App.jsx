cd1ce801b82fc107704f363a5663d754db636414
ibricks-bizdev <jiminlee@i-bricks.co.kr>
2024-03-14 14:27:25 +0900

    feat: 상품 목록 화면 구현

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,3 +1,31 @@
-const App = () => <>1-react</>;
-
+const App = () =>
+    <div className="ProductPage">
+        <div className="Page">
+            <header>
+                <h1>메뉴목록</h1>
+            </header>
+            <main>
+                <ul>
+                    <li>
+                        <div className="ProductItem">
+                            <div className="description">
+                                <h2>고소한바질파스타</h2>
+                                <div>5,500원</div>
+                                <button className="Button brand">주문하기</button>
+                            </div>
+                            <div className="thumbnail">
+                                <img src="./images/menu-고소한바질파스타.jpg" alt="" />
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </main>
+            <footer>
+                <nav className="Navbar">
+                    <a className="active" href="#">메뉴목록</a>
+                    <a className="active" href="#">주문내역</a>
+                </nav>
+            </footer>
+        </div>
+    </div>
 export default App;
